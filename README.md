c2002529b409e2a0cdbee8c2aa349d0f66b6070a
AkienMain <148787800+AkienMain@users.noreply.github.com>
2026-01-13 01:02:14 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+
+Generate a set of subsets of combination.
+
+
+input
+``` {js}
+nCrSubsets(4, 2);
+```
+
+return
+```
+[[0,1], [0,2], [0,3], [1,2], [1,3], [2,3]]
+```
---
+++ ncr-subsets.js
@@ -0,0 +1,28 @@
+import isInteger from "number.isinteger";
+
+// combinations: n choose r (nCr)
+function nCrSubsets(n, r) {
+  if (
+    (isInteger(n) && isInteger(r)) &&
+    (n >= 0 && r >= 0) &&
+    n >= r
+  ) {
+    return nCrSubsetsRecursive(n, r, [], []);
+  } else {
+    return undefined;
+  }
+}
+
+function nCrSubsetsRecursive(n, r, subset, setOfSubsets) {
+  let initial = subset.length > 0 ? subset[subset.length-1]+1 : 0;
+  for (let i=initial; i<n; i++) {
+    let s = [...subset];
+    s.push(i)
+    if (s.length == r) {
+      setOfSubsets.push(s);
+    } else {
+      nCrSubsetsRecursive(n, r, s, setOfSubsets)
+    }
+  }
+  return setOfSubsets
+}
